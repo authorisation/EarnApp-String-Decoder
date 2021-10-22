86ee09d6883b6c198142786a9a3a45d817582390
auth <64337177+authorisation@users.noreply.github.com>
2021-10-22 16:54:00 +0200

    Add files via upload

---
+++ EarnApp String Decoder/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace EarnApp_String_Decoder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "made in china (by auth Ltd.)";
+            Console.WriteLine("made by auth mit liebe");
+            Console.WriteLine("string :");
+            int bufSize = 10000;
+            Stream inStream = Console.OpenStandardInput(bufSize);
+            Console.SetIn(new StreamReader(inStream, Console.InputEncoding, false, bufSize));
+
+            string line = Console.ReadLine();
+            if(line == null) { return; }
+                try {
+                string decoded = line.decode();
+                Console.WriteLine("\n");
+                Console.WriteLine(decoded);
+                }
+                catch (Exception e) {
+                Console.WriteLine("invalid string");
+                }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ EarnApp String Decoder/util.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+
+namespace EarnApp_String_Decoder
+{
+    public static class util
+    {
+        public static string decode(this string s)
+        {
+            int num = s.IndexOf('=');
+            int length = s.Length;
+            string s2;
+            if (num == -1)
+            {
+                s2 = s.Substring(length - 3) + s.Substring(0, length - 3);
+            }
+            else
+            {
+                s2 = s.Substring(num - 3, 3) + s.Substring(0, num - 3) + s.Substring(num);
+            }
+            try
+            {
+                return Encoding.UTF8.GetString(Convert.FromBase64String(s2));
+            }
+            catch
+            {
+            }
+            return null;
+        }
+    }
+}
